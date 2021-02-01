d66fc2e8030f56140005e26a9740ef7d5416f82b
ihux <>
2021-02-01 17:35:58 +0100

    fine tune and test SPM toolbox - good status :-)))

--- swarovski/spm/V1e1/@spm/version.m
+++ swarovski/spm/V1e1/@spm/version.m
@@ -195,7 +195,7 @@ function vers = version(o,arg)         % SPM Class Version
 % - move K0,f0,S0,T0 into loop segment
 % - spm/stable method to work with options
 % - add phi parameter for coordinate transformation
-% - fine tune and test SPM toolbox
+% - fine tune and test SPM toolbox - good status :-)))
 %
 % Known bugs & wishlist
 % =========================
