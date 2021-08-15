8b449c33b11cff37df90f0e800cfcc465846ac03
ihux <hugo.pristauz@gmail.com>
2021-08-15 13:53:10 +0200

    add work/@ctrl test class for wrapping MATLAB control toolbox

---
+++ work/@ctrl/analyse.m
@@ -0,0 +1,113 @@
+function oo = analyse(o,varargin)     % Graphical Analysis
+%
+% ANALYSE   Graphical analysis
+%
+%    Plenty of graphical analysis functions
+%
+%       analyse(o)                % analyse @ opt(o,'mode.analyse')
+%
+%       oo = analyse(o,'menu')    % setup Analyse menu
+%       oo = analyse(o,'Surf')    % surface plot
+%       oo = analyse(o,'Histo')   % display histogram
+%
+%    See also: CTRL, PLOT, STUDY
+%
+   [gamma,o] = manage(o,varargin,@Error,@Menu,@WithCuo,@WithSho,...
+                                 @WithBsk,@Surf,@Histo);
+   oo = gamma(o);                 % invoke local function
+end
+
+%==========================================================================
+% Menu Setup & Common Menu Callback
+%==========================================================================
+
+function oo = Menu(o)
+   oo = mitem(o,'Surface',{@WithCuo,'Surf'},[]);
+   oo = mitem(o,'Histogram',{@WithCuo,'Histo'},[]);
+end
+
+%==========================================================================
+% Launch Callbacks
+%==========================================================================
+
+function oo = WithSho(o)               % 'With Shell Object' Callback  
+%
+% WITHSHO General callback for operation on shell object
+%         with refresh function redefinition, screen
+%         clearing, current object pulling and forwarding to executing
+%         local function, reporting of irregularities, dark mode support
+%
+   refresh(o,o);                       % remember to refresh here
+   cls(o);                             % clear screen
+ 
+   gamma = eval(['@',mfilename]);
+   oo = gamma(o);                      % forward to executing method
+
+   if isempty(oo)                      % irregulars happened?
+      oo = set(o,'comment',...
+                 {'No idea how to plot object!',get(o,{'title',''})});
+      message(oo);                     % report irregular
+   end
+   dark(o);                            % do dark mode actions
+end
+function oo = WithCuo(o)               % 'With Current Object' Callback
+%
+% WITHCUO A general callback with refresh function redefinition, screen
+%         clearing, current object pulling and forwarding to executing
+%         local function, reporting of irregularities, dark mode support
+%
+   refresh(o,o);                       % remember to refresh here
+   cls(o);                             % clear screen
+ 
+   oo = current(o);                    % get current object
+   gamma = eval(['@',mfilename]);
+   oo = gamma(oo);                     % forward to executing method
+
+   if isempty(oo)                      % irregulars happened?
+      oo = set(o,'comment',...
+                 {'No idea how to plot object!',get(o,{'title',''})});
+      message(oo);                     % report irregular
+  end
+  dark(o);                            % do dark mode actions
+end
+function oo = WithBsk(o)               % 'With Basket' Callback        
+%
+% WITHBSK  Plot basket, or perform actions on the basket, screen clearing, 
+%          current object pulling and forwarding to executing local func-
+%          tion, reporting of irregularities and dark mode support
+%
+   refresh(o,o);                       % use this callback for refresh
+   cls(o);                             % clear screen
+
+   gamma = eval(['@',mfilename]);
+   oo = basket(o,gamma);               % perform operation gamma on basket
+ 
+   if ~isempty(oo)                     % irregulars happened?
+      message(oo);                     % report irregular
+   end
+   dark(o);                            % do dark mode actions
+end
+
+%==========================================================================
+% Actual Analysis
+%==========================================================================
+
+function o = Surf(o)                   % Surf Plot
+   x = cook(o,'x');
+   y = cook(o,'y');
+
+   idx = 1:ceil(length(x)/50):length(x);
+   idy = 1:ceil(length(y)/50):length(y);
+   z = x(idx)'.*y(idy);
+   surf(x(idx),y(idy),z);
+end
+function o = Histo(o)                  % Histogram
+   t = cook(o,':');
+   x = cook(o,'x');
+   y = cook(o,'y');
+
+   subplot(211);
+   plot(with(corazon(o),'style'),t,sort(x),'r');
+   subplot(212);
+   plot(with(corazon(o),'style'),t,sort(y),'b');
+end
